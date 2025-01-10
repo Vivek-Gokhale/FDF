0e44bd179ca06fdb352189e45448947e5d9d0bb5
Vivek-Gokhale <vivekgokhale23@gmail.com>
2025-01-10 09:36:51 +0530

    done

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Filters = ({ onFilter, applyNoFilters, sortByDateAsc, sortByDateDesc, sortByNIAsc, sortByNIDesc, sortByRevAsc, sortByRevDesc}) => {
+const Filters = ({ onFilter, applyNoFilters, sortByDateAsc, sortByDateDesc, sortByNIAsc, sortByNIDesc, sortByRevAsc, sortByRevDesc }) => {
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
     const [minRevenue, setMinRevenue] = useState("");
@@ -27,31 +27,42 @@ const Filters = ({ onFilter, applyNoFilters, sortByDateAsc, sortByDateDesc, sort
         }
     };
 
+    const HandleapplyNoFilters = ()=>{
+        setStartDate("");
+        setEndDate("");
+        setMaxNetIncome("");
+        setMinNetIncome("");
+        setMaxRevenue("");
+        setMinRevenue("");
+        applyNoFilters();
+    }
+
 
 
     return (
         <>
             <div id="popup-modal" className={`${modalVisible ? 'block' : 'hidden'} overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex justify-center items-center`}>
                 <div className="relative p-4 w-full max-w-md max-h-full">
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <button type="button" className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={closeModal}>
-                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span className="sr-only">Close</span>
+                <div className="relative bg-white rounded-lg shadow-2xl dark:bg-gray-700">
+
+                    <button type="button" className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={closeModal}>
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span className="sr-only">Close</span>
+                    </button>
+                    <div className="p-4 md:p-5 text-center">
+                        <svg className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Start date, min revenue or min net income cannot be greater than min
+                        </h3>
+                        <button onClick={closeModal} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            Close
                         </button>
-                        <div className="p-4 md:p-5 text-center">
-                            <svg className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                            </svg>
-                            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                Start date, min revenue or min net income cannot be greater than min
-                            </h3>
-                            <button onClick={closeModal} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                Close
-                            </button>
-                        </div>
                     </div>
+                </div>
                 </div>
             </div>
 
@@ -135,10 +146,10 @@ const Filters = ({ onFilter, applyNoFilters, sortByDateAsc, sortByDateDesc, sort
                     </button>
                     <button
                         style={{ backgroundColor: '#1E712A', color: 'white' }}
-                        onClick={applyNoFilters}
+                        onClick={HandleapplyNoFilters}
                         className="flex-grow bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
                     >
-                        No Filters
+                        Clear Filters
                     </button>
                     <button
                         style={{ backgroundColor: '#1E712A', color: 'white' }}
